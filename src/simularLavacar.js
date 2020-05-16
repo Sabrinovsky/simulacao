c1a75faba95a9e66381ef9e7beb35c92ba8ee63c
Matheus <matheusouzatj@gmail.com>
2020-05-16 18:15:32 -0300

    arrumei os tempos de entrar e sair dos servicos

--- src/simularLavacar.js
+++ src/simularLavacar.js
@@ -6,36 +6,40 @@ export function simular ({ horas, chegadaDeterministica, tempoServicoDeterminist
   let proximoCliente = parseInt(chegadaDeterministica)
 
   const intervaloServico = parseInt(tempoServicoDeterministico)
-  let terminoProximoServico = parseInt(tempoServicoDeterministico)
+  let terminoProximoServico = undefined
 
   for (let i = 0; i <= minutos; i++) {
-    if (i == proximoCliente) {
-      proximoCliente += intervaloChegada
-      alocaCliente({id:i})
+    if(funcionario.atendendo && i === terminoProximoServico ){
+      terminaServico(i)
     }
 
-    if(!!funcionario.atendendo && i == terminoProximoServico ){
-      terminoProximoServico += intervaloServico
-      terminaServico({id:i})
+    if (i == proximoCliente) {
+      proximoCliente += intervaloChegada
+      alocaCliente({id:i},i)
     }
+
+    console.log(terminoProximoServico)
+
   }
 
   console.log(funcionario)
 
-  function alocaCliente(cliente){
+  function alocaCliente(cliente,tempoAtual){
     if(funcionario.atendendo){
       const novaFila = [...funcionario.fila, cliente]
       funcionario = {...funcionario, fila: novaFila}
     }else{
       funcionario = {...funcionario, atendendo: cliente}
+      terminoProximoServico = intervaloServico + tempoAtual
     }
   }
 
-  function terminaServico(){
+  function terminaServico(tempoAtual){
     if(funcionario.fila.length > 0){
       const novaFila = funcionario.fila.filter((_,index) => index !== 0)
 
       funcionario = {...funcionario,atendendo:funcionario.fila[0], fila: novaFila}
+      terminoProximoServico = intervaloServico + tempoAtual
     }else{
       funcionario = {...funcionario, atendendo: false}
     }
