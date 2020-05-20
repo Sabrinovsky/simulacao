8eb66cfa8190c78969dbf6bc156d52685ca6d5b1
Matheus <matheusouzatj@gmail.com>
2020-05-19 22:56:39 -0300

    teste

---
+++ build/precache-manifest.2c211e0581a72cd5d40420d63506d43a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1ef92cf7852038eef666d082233da5",
+    "url": "/simulacao/index.html"
+  },
+  {
+    "revision": "7729c6c9f79058068be9",
+    "url": "/simulacao/static/css/main.9c57c1c7.chunk.css"
+  },
+  {
+    "revision": "88cab4566a84c031c445",
+    "url": "/simulacao/static/js/2.7e122d5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simulacao/static/js/2.7e122d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7729c6c9f79058068be9",
+    "url": "/simulacao/static/js/main.01be9b66.chunk.js"
+  },
+  {
+    "revision": "cf70692549742fe6c5d5",
+    "url": "/simulacao/static/js/runtime-main.f9b4c3d2.js"
+  }
+]);
